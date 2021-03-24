6e4e263e0b1ec6e42b459e0868a07aa523d87125
philwoz <61164019+philwoz@users.noreply.github.com>
2021-03-24 10:13:06 +0000

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+ const [list, setList] = useState([]);
+ const [input, setInput] = useState("")
+
+
+ 
+
+ 
+  
+
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setList([...list, input])
+    setInput("")
+  }
+
+  const handleDelete = (i) => {
+    let tempArr = [...list]
+    tempArr.splice(i, 1)
+    setList(tempArr)
+  }
+
+  const handleEdit = (i) => {
+    let tempArr = [...list]
+    tempArr[i] = input
+    setList(tempArr)
+    setInput("")
+  }
+
+
+
+  
+    return (
+      <div className="App">
+        {/* <button onClick={() => setList([...list,1])}></button> */}
+        <form className="input-wrapper" onSubmit={handleSubmit}>
+          <input className="todotxt" type="text" placeholder="Add todo" onChange={(event) => setInput(event.target.value)} value={input} />
+          {/* <button className="submit" type="submit" value="submit" >Add Todo</button> */}
+        </form>
+        {list.map((item, i) => (
+          <div className="todo-element">
+            <input type="checkbox" value="complete" />
+          <p>{item}</p>
+          <button onClick={() => handleDelete(i)}>Delete</button>
+          <button onClick={() => handleEdit(i)}>Edit</button>
+          </div>
+        ))}
+    
+      </div>
+    )
+  }
+
+
+
+// const TodoEntry = (props) => {
+
+
+   
 
 export default App;
